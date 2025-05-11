8a25ca024788e6a18afd0db4f9ec27b738485838
修叶 <3452630248@qq.com>
2025-05-11 13:51:19 +0800

    1

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -59,20 +59,20 @@ const router = new VueRouter({
   routes,
 });
 
-// 添加全局前置守卫
-router.beforeEach((to, from, next) => {
-  const accessToken = localStorage.getItem("accessToken");
-
-  // 如果要访问的是登录页，直接放行
-  if (to.name === "Login") {
-    return next();
-  }
-  // 如果没有token，重定向到登录页
-  if (!accessToken) {
-    return next({ name: "Login" });
-  }
-  // 否则继续导航
-  next();
-});
+// // 添加全局前置守卫
+// router.beforeEach((to, from, next) => {
+//   const accessToken = localStorage.getItem("accessToken");
+//
+//   // 如果要访问的是登录页，直接放行
+//   if (to.name === "Login") {
+//     return next();
+//   }
+//   // 如果没有token，重定向到登录页
+//   if (!accessToken) {
+//     return next({ name: "Login" });
+//   }
+//   // 否则继续导航
+//   next();
+// });
 
 export default router;
